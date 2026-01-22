d68c1790f0a507c59799228114e6f486677597fd
Fauzian <emailkamu@example.com>
2026-01-22 19:47:14 +0700

    back to beginning

--- app.js
+++ app.js
@@ -1,53 +1,13 @@
-const today = new Date().toISOString().slice(0,10);
-
 const db = JSON.parse(localStorage.getItem("mbg")) || {
-  dailyStock: null,
   incoming: [],
   distribution: [],
   returns: []
 };
 
-
 function save() {
   localStorage.setItem("mbg", JSON.stringify(db));
   renderDashboard();
   renderReport();
-}
-
-function saveDailyStock() {
-  const qty = Number(dailyQty.value);
-  if (!qty) return alert("Quantity required");
-
-  db.dailyStock = {
-    date: today,
-    qty,
-    note: dailyNote.value
-  };
-
-  save();
-  unlockApp();
-}
-
-function checkDailyReset() {
-  if (!db.dailyStock || db.dailyStock.date !== today) {
-    db.dailyStock = null;
-    lockApp();
-    save();
-  }
-}
-
-function lockApp() {
-  document.querySelectorAll(".sidebar nav button")
-    .forEach(b => b.disabled = true);
-
-  dailyStockBox.classList.remove("d-none");
-}
-
-function unlockApp() {
-  document.querySelectorAll(".sidebar nav button")
-    .forEach(b => b.disabled = false);
-
-  dailyStockBox.classList.add("d-none");
 }
 
 function showPage(id, btn) {
@@ -59,12 +19,9 @@ function showPage(id, btn) {
 }
 
 function stock() {
-  if (!db.dailyStock) return 0;
-
-  return db.dailyStock.qty
-    + db.incoming.reduce((a,b)=>a+b.qty,0)
-    - db.distribution.reduce((a,b)=>a+b.qty,0)
-    + db.returns.reduce((a,b)=>a+b.qty,0);
+  return db.incoming.reduce((a,b)=>a+b.qty,0)
+       - db.distribution.reduce((a,b)=>a+b.qty,0)
+       + db.returns.reduce((a,b)=>a+b.qty,0);
 }
 
 function addIncoming() {
@@ -84,12 +41,10 @@ function addReturn() {
 }
 
 function renderDashboard() {
-  if (db.dailyStock) {
-    dailyQty.value = db.dailyStock.qty;
-    dailyNote.value = db.dailyStock.note;
-  }
-
   dStock.textContent = stock();
+  dIn.textContent = db.incoming.reduce((a,b)=>a+b.qty,0);
+  dOut.textContent = db.distribution.reduce((a,b)=>a+b.qty,0);
+  dReturn.textContent = db.returns.reduce((a,b)=>a+b.qty,0);
 }
 
 function renderReport() {
@@ -110,6 +65,5 @@ function renderReport() {
    });
 }
 
-checkDailyReset();
 renderDashboard();
 renderReport();
